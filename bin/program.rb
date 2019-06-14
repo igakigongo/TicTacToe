b63949fd40ce32058ced5454afef42c9d3eccafb
Edward Iga Kigongo <igakigongo@gmail>
2019-06-14 17:40:31 +0200

    Add player class with the ability to store a symbol and player's name

---
+++ bin/program.rb
@@ -0,0 +1,6 @@
+require_relative "../lib/player"
+
+player = Player.new("Jason", "k")
+
+p player
+player.play
---
+++ lib/board.rb
@@ -0,0 +1,18 @@
+class board
+    def initialize(size)
+        
+    end
+end
+
+board = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]]
+
+size = n
+max_times_to_play = n**2
+
+# board class
+# add_nought_or_cross
+# won? - let us know if the game is won
+
