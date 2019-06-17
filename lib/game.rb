2acc7423e365951af0c3938b6b65d2e9c607aaf4
Moses Ogwo <mosesogwo@gmail.com>
2019-06-17 11:53:09 +0100

    separate methods for getting and validating choice

--- lib/game.rb
+++ lib/game.rb
@@ -25,7 +25,7 @@ class Game
         n = 0
         while n < @@max_moves
 						puts "Make a choice between 1 - 9"
-						choice = get_and_validate_choice
+						choice = get_choice
 						@board.recieve_choice(choice, @current_player)
 						puts @board.to_s
             @current_player = @current_player == @player_one ? @player_two : @player_one
@@ -33,17 +33,26 @@ class Game
         end
 		end
 
-		def get_and_validate_choice
-			choice = gets.chomp.to_i
+		def get_choice
+			choice = nil
 				while true
+					choice = validate_choice
 					break if @board.choice_free?(choice)
 					puts "Choice cell is not free"
 					puts "Make another choice between 1 - 9"
-					choice = gets.chomp.to_i
 				end
-			choice
+			choice.to_i
 		end
 
+		def validate_choice
+			choice = gets.chomp
+			while true
+				break if (/^[1-9]{1}$/).match?(choice)
+				puts "Invalid Choice. Enter a number between 1 and 9"
+				choice = gets.chomp
+			end
+			choice.to_i
+		end
 		
 		
 			
