25b10f49cd2cabc74e064c1f0baddb6dae19b9ab
Edward Iga Kigongo <igakigongo@gmail>
2019-06-17 15:52:20 +0200

    Remove dummy contents in program file

--- bin/program.rb
+++ bin/program.rb
@@ -2,8 +2,6 @@ require_relative "../lib/board"
 require_relative "../lib/game"
 require_relative "../lib/player"
 
-# state is manual, will be updated to pick results 
-# using the console 
 board = Board.new
 player_one = Player.new("Moses", "X")
 player_two = Player.new("Edward", "O")
